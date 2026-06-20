b918fd55e1b38dd37087f055b709095d40478c4d
silmisalsabila <silmiyusniah@gmail.com>
2026-06-20 10:07:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,8 +15,14 @@ st.set_page_config(
 # JUDUL APLIKASI
 # ==================================================
 
-st.title("Eco-Forest Valuation")
-st.subheader("Pembelajaran Ekonomi Sumber Daya Hutan")
+col1, col2 = st.columns([1, 6])
+
+with col1:
+    st.image("Logo_Unisba.png", width=100)
+
+with col2:
+    st.title("Eco-Forest Valuation")
+    st.subheader("Pembelajaran Ekonomi Sumber Daya Hutan")
 
 # ==================================================
 # SIDEBAR MENU
